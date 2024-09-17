b6748989fb723bc431a3469d5681b588d8056343
Hieu Nguyen <nhthieu16@proton.me>
2024-09-17 15:23:08 +0700

    change config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,32 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  opts = {
-    ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
-    auto_install = false,
-    highlight = {
-      enable = true,
-      -- disable = { 'lua', 'go', 'rust', 'c' },
-      disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-          return true
-        end
-      end,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true },
-    autotag = {
-      enable = true,
-    },
-  },
-  config = function(_, opts)
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    ---@diagnostic disable-next-line: missing-fields
-    require('nvim-treesitter.configs').setup(opts)
-    -- folding
-    -- vim.wo.foldmethod = 'expr'
-    -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	opts = {
+		ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
+		auto_install = false,
+		highlight = {
+			enable = true,
+			-- disable = { 'lua', 'go', 'rust', 'c' },
+			disable = function(lang, buf)
+				local max_filesize = 100 * 1024 -- 100 KB
+				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+				if ok and stats and stats.size > max_filesize then
+					return true
+				end
+			end,
+			additional_vim_regex_highlighting = false,
+		},
+		indent = { enable = true },
+		autotag = {
+			enable = true,
+		},
+	},
+	config = function(_, opts)
+		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+		---@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter.configs").setup(opts)
+		-- folding
+		-- vim.wo.foldmethod = 'expr'
+		-- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+	end,
 }
