083602d0e8bf7843ee0df5ce7a0ca2d8f8671547
nibtr <nhthieu16@proton.me>
2025-04-11 21:57:24 +0700

    adjust cmp

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -38,13 +38,28 @@ return {
 
       luasnip.config.setup({})
       cmp.setup({
+        performance = {
+          debounce = 0, -- default is 60ms
+          throttle = 0, -- default is 30ms
+        },
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
           end,
         },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
         completion = { completeopt = 'menu,menuone,noinsert' },
-        mapping = cmp.mapping.preset.insert {
+        matching = {
+          disallow_fuzzy_matching = true,
+          disallow_fullfuzzy_matching = true,
+          disallow_partial_fuzzy_matching = true,
+          disallow_partial_matching = false,
+          disallow_prefix_unmatching = true,
+        },
+        mapping = cmp.mapping.preset.insert({
           ['<C-n>'] = cmp.mapping.select_next_item(),
           ['<C-p>'] = cmp.mapping.select_prev_item(),
           ['<C-u>'] = cmp.mapping.scroll_docs(4),  -- scroll up preview
@@ -63,7 +78,7 @@ return {
             end
           end, { 'i', 's' }),
           ['<C-e>'] = cmp.mapping.abort(),
-        },
+        }),
         sources = {
           {
             name = 'lazydev',
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -7,9 +7,10 @@ return {
     highlight = {
       enable = true,
       -- disable = { 'lua', 'go', 'rust', 'c' },
+      ---@diagnostic disable-next-line: unused-local
       disable = function(lang, buf)
         local max_filesize = 100 * 1024 -- 100 KB
-        ---@diagnostic disable-next-line: undefined-global
+        ---@diagnostic disable-next-line: undefined-field
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
           return true
